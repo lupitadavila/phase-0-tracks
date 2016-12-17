8c26d04af21bcb99bd0222d2a6fa999395b2c908
Lupita Davila <lupita.dav@gmail.com>
2016-12-17 12:24:47 -0800

    Add method that returns fibonnaci sequence array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -21,3 +21,19 @@ arr = [42,89,23,1]
 p search_array(arr,1)
 p search_array(arr,24)
 
+def fib_fibonacci(numbers)
+	arr = [0,1] 
+	sum = 0
+	until arr.length == numbers
+		#add last two numbers
+		sum = arr[arr.length-1] + arr[arr.length-2]
+
+		#push that sum to the array
+		arr.push(sum)
+	end
+	arr
+end
+p fib_fibonacci(6)
+
+check = fib_fibonacci(100)
+puts check.last
