b81379bf525d26aacbc838610ac2a5bfbe8819a6
Lupita Davila <lupita.dav@gmail.com>
2016-12-18 08:24:04 -0800

    Update to bubble sort method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -49,42 +49,19 @@ puts check.last
 # step 7: else number is not swapped, than number is sorted, break the loop.
 
 
-def bubble_sort(array) 
-  n = array.length
-  loop do
-    swapped = false
-
-    (n-1).times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
-        swapped = true
-      end
-    end
-
-    break if not swapped
-  end
-
-  array
-end
-
-array1 = [1,45,99,2,87,100,3]
-p bubble_sort(array1)
 def bubble_sort(array)
-  n = array.length
-  loop do
-    swapped = false
+	loop do
+		swapped = false
+		(array.length-1).times do |i|
+		  if array[i] > array[i+1]
+		    array[i], array[i+1] = array[i+1], array[i]
+		    swapped = true
+		  end
+		end
 
-    (n-1).times do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
-        swapped = true
-      end
-    end
-
-    break if not swapped
-  end
-
-  array
+		break if not swapped
+	end
+	array
 end
 
 array1 = [1,45,99,2,87,100,3]
