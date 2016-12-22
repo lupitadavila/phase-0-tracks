af13f375b5b9516c9bf9c29e43e79ac51901bac2
Lupita Davila <lupita.dav@gmail.com>
2016-12-21 19:52:20 -0800

    Add methods to Puppy class and drivercode

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(num_of_times)
+  	num_of_times.times do
+  		puts "Woof!"
+  	end
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	puts human_years*7
+  	human_years*7
+  end
+
+  def stay(seconds)
+  	puts "*stays for #{seconds} seconds*"
+  end
+
 end
 
+#initialize instance of Puppy
+skip = Puppy.new
+
+skip.fetch("ball")
+skip.speak(3)
+skip.roll_over
+skip.dog_years(2)
+skip.stay(10)
