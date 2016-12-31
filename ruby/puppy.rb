fc2f553d8fb2cd25cd3cc9c2ba9598fb404976e5
Lupita Davila <lupita.dav@gmail.com>
2016-12-30 19:28:24 -0800

    add characteristics and details of puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+#SPECIES ------------------------
+#Puppy
+
+#CHARACTERISTICS ----------------
+#Eye Color: varies
+#Friendly: yes
+#Leg count: 4
+#Name: varies
+#Color: varies
+
+#BEHAVIOR -----------------------
+#wag tail
+#bark
+#fetch
+
+class Puppy
+end
