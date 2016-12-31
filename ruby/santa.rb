1470129e670f044d68e4fd94b391b52c42f2b9c6
Lupita Davila <lupita.dav@gmail.com>
2016-12-30 21:07:10 -0800

    Add diverse initializations to file

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
 
-	def initialize
-		puts "Initializing Santa instance"
+	def initialize(gender,ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer",
+			"Prancer", "Vixen", "Comet", "Cupid", "Donner",
+			"Blitzen"]
+		@age= 0
 	end
 
 	def speak
@@ -13,7 +18,23 @@ class Santa
 	end
 end
 
-santa_test = Santa.new
+#santa_test = Santa.new
+#santa_test.speak
+#santa_test.eat_milk_and_cookies("snickerdoodle")
 
-santa_test.speak
-santa_test.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+more_santas = {}
+example_names = ["Sam", "Tom", "Jay", "Nelly", "Bobbie", "Sean", "Oliver"]
+
+example_genders.length.times do |i|
+  more_santas[example_names[i]] = Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p more_santas
