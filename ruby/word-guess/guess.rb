baafdb0bc9c02c037fcd79af51aac6c27bf4bbf6
Lupita Davila <lupita.dav@gmail.com>
2017-01-01 12:23:55 -0800

    Update methods in Word guess class

---
+++ ruby/word-guess/guess.rb
@@ -0,0 +1,113 @@
+# Word guessing game
+
+class WordGuess
+	#getter
+	attr_reader :num_of_guesses, :board_array
+	#no setters
+
+	#### Method initialize word
+	# @word_array - word and sets it as array
+	# @board_array - initialzie empty board array the same length as word
+	# @num_of_guesses - total number of guesses set to 0 by default
+	# @is_over - true or false if game is over or not
+
+	def initialize(word)
+
+		@word_array = word.split('')
+		@board_array = []
+
+		#Set board
+		@word_array.length.times do
+			@board_array << "_"
+		end
+
+		@num_of_guesses = 0
+		@is_over = false
+
+	end
+
+	#### Method guess_letter?
+	#Searches for letter in array, if found, mark board
+	#Returns true or false if letter is found
+	def guess_letter?(letter)
+
+		#Add 1 to the total number of guesses in game IF not already guessed (in the board array)
+		if @board_array.index(letter) == nil
+			@num_of_guesses += 1
+		end
+
+		#If the letter exists (returns valid index)
+		# 1. Loop through letters in word and compare to the guessed letter
+		# 2. If guessed letter = original letter in word, set board = to letter in that index
+		# 3. Return True
+		#Else if the letter does not exist in word array
+		# 1. Return False
+		if @word_array.index(letter)
+			index = 0
+			@word_array.each do|i|
+				if letter == i
+					@board_array[index] = letter
+				end
+				index+=1
+			end
+			true
+		else
+			false
+		end
+	end
+
+
+	#### Method game_over?
+	#Check if game is over, return true or false
+	def game_over?
+
+		#If the number of guesses is greater or equal to the word length
+		# 1. Game is over, let user know they made too many guesses
+		# 2. @is_over = true
+		#Elsif the board and the word arrays are equal to each other
+		# 1. Game is over, player won, let user know
+		# 2. @is_over = true
+		#Else
+		# 1. @is_over = false
+
+		if @num_of_guesses >= @word_array.length
+			puts "Too many guesses! Game over."
+			@is_over = true
+
+		elsif @word_array.join == @board_array.join
+			puts "You won the game! Game over."
+			@is_over = true
+		else
+			@is_over = false
+		end
+	end
+
+	def print_board
+		@board_array.each do |char|
+			print "#{char} "
+		end
+		puts ""
+	end
+
+end
+
+# --------------------- DRIVER CODE
+
+puts "\n***************************************\n\nWelcome to the Word Guessing Game!\n\n***************************************\n"
+puts "\nPlayer 1, please enter your word:\n"
+word = gets.chomp
+puts "Awesome, Player 2 will have #{word.length} guesses\nRepeated guesses will not be counted against them.\n\n"
+game = WordGuess.new(word)
+guess = ""
+
+while (!game.game_over?)
+	game.print_board
+	puts "\nPlayer 2, guess a letter!"
+	guess = gets.chomp
+	if game.guess_letter?(guess)
+		puts "'#{guess}' is correct!"
+	else
+		puts "Incorrect guess!"
+		puts "Your current number of guesses = "+ game.num_of_guesses.to_s + "\n"
+	end
+end
