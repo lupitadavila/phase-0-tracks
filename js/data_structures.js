fbc27e186c8b44b50abab0e1399dcd9c766a9de0
Lupita Davila <lupita.dav@gmail.com>
2017-01-05 19:23:22 -0800

    Create constructor function for Car object

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,26 @@ if(names.length == colors.length){
   console.log("Arrays are different lengths")
 }
 console.log(horses)
+
+function Car(color, mpg, year, isLemon){
+  this.color = color;
+  this.mpg = mpg;
+  this.year = year;
+  this.isLemon = isLemon;
+
+  this.start = function() {console.log("vroom!!");}
+  
+}
+
+var car1 = new Car("red",32,1991,true);
+var car2 = new Car("blue",27,2012,false);
+var car3 = new Car("silver",41,2017,false);
+
+console.log(car1)
+car1.start();
+
+console.log(car2)
+car2.start();
+
+console.log(car3)
+car3.start();
