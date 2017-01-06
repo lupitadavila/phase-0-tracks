e8e93f7d6d991edddbe1bde5fbc2c5812d964f5b
Lupita Davila <lupita.dav@gmail.com>
2017-01-05 19:02:36 -0800

    Create js object using arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,13 @@ var names = ["Bertha", "Ed", "Shock", "Eliza"];
 colors.push("purple");
 names.push("Boo");
 
+var horses = {};
+
+if(names.length == colors.length){
+  for (var i = 0; i < names.length; i++) {
+  	horses[names[i]] = colors[i]
+  }
+}else{
+  console.log("Arrays are different lengths")
+}
+console.log(horses)
