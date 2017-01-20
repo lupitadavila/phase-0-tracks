3760a4504f4d9a757731848046790bd3eedc986f
Lupita Davila <lupita.dav@gmail.com>
2017-01-19 18:19:58 -0800

    Create ERB template and Http route on app.rb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader"
 require 'sqlite3'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
@@ -16,12 +17,24 @@ end
 get '/students/new' do
   erb :new_student
 end
+# http route for new page
+get '/students/remove' do
+  @students = db.execute("SELECT * FROM students")
+  erb :student_removal
+end
 
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+
+# remove a student via form
+post '/remove' do
+	db.execute("DELETE FROM students WHERE name=?",[params['name']])
+	redirect '/'
 end
 
 # add static resources
