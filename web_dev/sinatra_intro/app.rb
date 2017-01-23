0e88be4842a95445f2f8bb71210cd16727febdfc
Lupita Davila <lupita.dav@gmail.com>
2017-01-22 23:56:15 -0800

    Add routes for contact, great_job, and adding numbers

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,5 +1,6 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader"
 require 'sqlite3'
 
 db = SQLite3::Database.new("students.db")
@@ -43,4 +44,23 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+# display address
+
+get '/contact' do
+  "The campus address is: 633 Folsom Street (at Hawthorne) 6th Floor, San Francisco, CA 94107"
+end
+# Say good job
+get '/great_job' do
+  if params[:name]
+    "Good job, #{params[:name]}!"
+  else
+    "Good job!"
+  end
+end
+# Add two numbers
+get '/:num_1/add/:num_2' do
+  sum = params[:num_1].to_i + params[:num_2].to_i
+  "#{params[:num_1]}+#{params[:num_2]} = " + sum.to_s
 end
